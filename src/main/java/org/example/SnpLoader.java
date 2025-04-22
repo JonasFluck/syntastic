c38c388ec39e0fef85c128dd2efbd2efc5d465d7
julius <Julius.Braitinger@kavisio.de>
2025-04-22 12:09:00 +0200

    now we can decide how many patients -> init 1 to 5 for testing

--- src/main/java/org/example/SnpLoader.java
+++ src/main/java/org/example/SnpLoader.java
@@ -10,49 +10,45 @@ import org.example.concepts.attributes.Snp;
 
 public class SnpLoader {
 
-    private static final String DB_URL;
-    private static final String DB_USER;
-    private static final String DB_PASSWORD;
+    public static Map<String, Map<String, Snp>> loadSnps(String configPath) {
+        Properties props = loadDbProperties(configPath);
+        String DB_URL = props.getProperty("db.url");
+        String DB_USER = props.getProperty("db.user");
+        String DB_PASSWORD = props.getProperty("db.password");
 
-    static {
-        Properties props = loadDbProperties();
-        DB_URL = props.getProperty("db.url");
-        DB_USER = props.getProperty("db.user");
-        DB_PASSWORD = props.getProperty("db.password");
-    }
-
-    public static Map<String, Map<String, Snp>> loadSnps() {
         Map<String, Map<String, Snp>> patientSnps = new HashMap<>();
-
         String query =
                 "SELECT g.patient_id, g.snp_id, s.chromosome, s.position, " +
                         "s.reference, s.alternative, g.genotype " +
                         "FROM genotype g " +
-                        "JOIN snps s ON g.snp_id = s.snp_id";
+                        "JOIN snps s ON g.snp_id = s.snp_id " +
+                        "WHERE g.patient_id = ?";
 
-        try (
-                Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-                PreparedStatement stmt = conn.prepareStatement(query);
-                ResultSet rs = stmt.executeQuery()
-        ) {
-            while (rs.next()) {
-                String patientId = rs.getString("patient_id");
-                String snpId = rs.getString("snp_id");
-                int chromosome = rs.getInt("chromosome");
-                int position = Integer.parseInt(rs.getString("position"));
-                char reference = rs.getString("reference").charAt(0);
-                char alternative = rs.getString("alternative").charAt(0);
-                String genotype = rs.getString("genotype");
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement stmt = conn.prepareStatement(query)) {
 
-                Snp snp = new Snp();
-                snp.setRsId(snpId);
-                snp.setChromosome(chromosome);
-                snp.setPosition(position);
-                snp.setRef(reference);
-                snp.setAlt(alternative);
-                snp.setExpression(genotype);
+            for (int patientId = 1; patientId <= 5; patientId++) {
+                stmt.setInt(1, patientId);
+                try (ResultSet rs = stmt.executeQuery()) {
+                    while (rs.next()) {
+                        String snpId = rs.getString("snp_id");
+                        int chromosome = rs.getInt("chromosome");
+                        int position = rs.getInt("position");
+                        char reference = rs.getString("reference").charAt(0);
+                        char alternative = rs.getString("alternative").charAt(0);
+                        String genotype = rs.getString("genotype");
 
-                patientSnps.computeIfAbsent(patientId, k -> new HashMap<>()).put(snpId, snp);
+                        Snp snp = new Snp();
+                        snp.setRsId(snpId);
+                        snp.setChromosome(chromosome);
+                        snp.setPosition(position);
+                        snp.setRef(reference);
+                        snp.setAlt(alternative);
+                        snp.setExpression(genotype);
+
+                        patientSnps.computeIfAbsent(String.valueOf(patientId), k -> new HashMap<>()).put(snpId, snp);
+                    }
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -62,13 +58,13 @@ public class SnpLoader {
         return patientSnps;
     }
 
-    private static Properties loadDbProperties() {
+    private static Properties loadDbProperties(String configPath) {
         Properties props = new Properties();
-        try (InputStream input = new FileInputStream("config/db.properties")) {
+        try (InputStream input = new FileInputStream(configPath)) {
             props.load(input);
         } catch (IOException e) {
             e.printStackTrace();
-            System.err.println("Failed to load DB config.");
+            System.err.println("Failed to load DB config from: " + configPath);
         }
         return props;
     }
